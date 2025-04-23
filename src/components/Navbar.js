e643a93a6bb5212782eb7d020a3098729d5b927f
gomez-alejo <alejito15c@gmail.com>
2025-04-23 10:22:19 -0500

    encale a GIt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
       <a href="#servicios">Servicios</a>
       <a href="#usuarios">Usuarios</a>
       <a href="#contacto">Contacto</a>
-      <a href="">GitHub</a>
+      <a href="https://github.com/gomez-alejo/actividad.git">GitHub</a>
     </nav>
   );
 }
